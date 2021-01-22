2b7599284da94527dde28d655e8491582d76ff42
Marc Litchfield <marc.litchfield@opensesame.com>
2021-01-22 10:43:34 -0800

    Revert "Select workspace command"
    This reverts commit 5adaecfeb2651b39d4664016b83b3e8a8209a7bc.

--- src/commands/select-workspace.ts
+++ src/commands/select-workspace.ts
@@ -1,21 +1,23 @@
 import {Command, flags} from '@oclif/command'
-import * as currentBranch from 'current-git-branch';
-import * as util from 'util';
-const exec = util.promisify(require('child_process').exec);
 
 export default class SelectWorkspace extends Command {
-  static description = 'Select a terraform workspace, or create it if it does not exist. If workspace name is not provided, use the current git branch.'
+  static description = 'describe the command here'
 
   static flags = {
     help: flags.help({char: 'h'}),
-    workspace: flags.string({char: 'w', description: 'workspace name. If not provided, the current git branch will be used (minus the part before a / character)'}),
+    name: flags.string({char: 'n', description: 'name to print'}),
+    force: flags.boolean({char: 'f'}),
   }
 
+  static args = [{name: 'file'}]
+
   async run() {
-    const {flags} = this.parse(SelectWorkspace)
-    const branch = currentBranch() as string;
-    const workspaceName = flags.workspace || branch.split('/').slice(-1)[0]
-    const command = `terraform workspace select ${workspaceName} || terraform workspace new ${workspaceName}`;
-    await exec(command);
+    const {args, flags} = this.parse(SelectWorkspace)
+
+    const name = flags.name ?? 'world'
+    this.log(`hello ${name} from C:\\OpenSesame\\core-build-library\\src\\commands\\select-workspace.ts`)
+    if (args.file && flags.force) {
+      this.log(`you input --force and --file: ${args.file}`)
+    }
   }
 }
