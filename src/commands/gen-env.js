adfaf29dbf22dc28e2e765f674e062f2c680373b
Kat <katheirne.bajno@opensesame.com>
2021-03-25 15:16:27 -0700

    add file default

--- src/commands/gen-env.js
+++ src/commands/gen-env.js
@@ -1,58 +1,53 @@
-const {Command, flags} = require('@oclif/command');
+const util = require('util');
+const chalk = require("chalk");
+const path = require('path');
+const exists = util.promisify(require('fs').exists);
+const { Command, flags } = require("@oclif/command");
 
 class GenEnvCommand extends Command {
   async run() {
-    const {flags} = this.parse(GenEnvCommand);
-    console.log(flags)
+    const { flags } = this.parse(GenEnvCommand);
+    const filePath = path.join(process.cwd(), flags.templateFile);
+
+    if (!(await exists(filePath))) {
+      this.warn(`No template file found at ${filePath}`);
+      return;
+    }
+
+    console.log(flags);
+    console.log(chalk.green("Operation successful.\n"));
   }
 }
 
 GenEnvCommand.description =
   "Template .env files with values from AWS Secrets, AWS SSM, and environment variables";
 
-  GenEnvCommand.flags = {
+GenEnvCommand.flags = {
   templateFile: flags.string({
     char: "f",
     description: "The file to template.",
+    default: ".env.tpl"
   }),
   profile: flags.string({
     char: "p",
-    description: "AWS Profile"
-  })
+    description: "AWS Profile",
+  }),
 };
 
 module.exports = GenEnvCommand;
-
-// const util = require("util");
-// const { Command, flags } = require("@oclif/command");
-// const JSONStream = require("JSONStream");
-// const es = require("event-stream");
-// const chalk = require("chalk");
-// const { clear } = require("console");
-// const {fs, exists } = require('fs');
-// const readline = require('readline');
-// const util = require('util');
-// const path = require('path');
-// const exec = util.promisify(require('child_process').exec);
-// const exists = util.promisify(require('fs').exists);
 
 // class GenEnvCommand extends Command {
 //   async run() {
 //     const { flags } = this.parse(GenEnvCommand);
 
 //     const filePath = path.join(process.cwd(), flags.templateFile);
-    
-//     if (!await exists(filePath)) {
-//       this.warn(`No template file found at ${filePath}`);
-//       return;
-//     }
 
 //     var rd = readline.createInterface({
 //         input: fs.createReadStream(filePath),
 //         output: process.stdout,
 //         console: false
 //     });
-    
+
 //     rd.on('line', function(line) {
 //         console.log(line);
 //     });
@@ -60,4 +55,3 @@ module.exports = GenEnvCommand;
 //     console.log(chalk.green("Operation successful.\n"));
 //   }
 // }
-
