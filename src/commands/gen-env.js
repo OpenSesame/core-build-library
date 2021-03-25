d3086ad63e19ad70a9d247974602b297c87e1e84
Kat <katheirne.bajno@opensesame.com>
2021-03-25 15:27:06 -0700

    read line by line

--- src/commands/gen-env.js
+++ src/commands/gen-env.js
@@ -1,14 +1,38 @@
-const util = require('util');
+const util = require("util");
 const chalk = require("chalk");
-const path = require('path');
+const path = require("path");
 // const readline = util.promisify(require('readline').exists);
-const fs = require('fs');
-const readline = require('readline');
-const exists = util.promisify(require('fs').exists);
+const fs = require("fs");
+const readline = require("readline");
+const exists = util.promisify(require("fs").exists);
 const { Command, flags } = require("@oclif/command");
 
 class GenEnvCommand extends Command {
   async run() {
+    const rules = [
+      {
+        pattern: /\$\{(?<variable>.*?)\}/,
+        replacer: async ({ variable }) => {
+          console.log("env:", variable);
+          return "ENV_VALUE";
+        },
+      },
+      {
+        pattern: /\{\{\s*aws_secret:(?<secretId>.*?):(?<jsonPath>.*?)\s*\}\}/,
+        replacer: async ({ secretId, jsonPath }) => {
+          console.log("secret:", secretId, jsonPath);
+          return "REPLACED_SECRET";
+        },
+      },
+      {
+        pattern: /\{\{\s*aws_ssm_param:(?<paramKey>.*?)\s*\}\}/,
+        replacer: async ({ paramKey }) => {
+          console.log("ssm:", paramKey);
+          return "REPLACED_PARAM";
+        },
+      },
+    ];
+
     const { flags } = this.parse(GenEnvCommand);
     const filePath = path.join(process.cwd(), flags.templateFile);
 
@@ -19,11 +43,19 @@ class GenEnvCommand extends Command {
 
     const rd = readline.createInterface({
       input: fs.createReadStream(filePath),
-      console: false
+      console: false,
     });
-      
-    rd.on('line', function(line) {
+
+    rd.on("line", function (line) {
       console.log(line);
+
+      for(const rule of rules) {
+        let match = line.match(rule.pattern);
+        const replacementValue = await rule.replacer(match.groups);
+        line = line.substr(0, match.index) + replacementValue + line.substr(match.index + match[0].length);
+      }
+      console.log(`before replace`)
+      console.log(line)
     });
 
     console.log(flags);
@@ -38,7 +70,7 @@ GenEnvCommand.flags = {
   templateFile: flags.string({
     char: "f",
     description: "The file to template.",
-    default: ".env.tpl"
+    default: ".env.tpl",
   }),
   profile: flags.string({
     char: "p",
@@ -53,7 +85,6 @@ module.exports = GenEnvCommand;
 //     const { flags } = this.parse(GenEnvCommand);
 
 //     const filePath = path.join(process.cwd(), flags.templateFile);
-
 
 //     console.log(chalk.green("Operation successful.\n"));
 //   }
